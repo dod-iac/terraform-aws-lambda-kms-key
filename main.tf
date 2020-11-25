9d37dd1c39e753b073d5d2b8ef5cd9e1321bf348
Chris Gilmer <chris.gilmer@gmail.com>
2020-11-25 10:21:31 -0800

    first commit

---
+++ main.tf
@@ -0,0 +1,88 @@
+/**
+ * ## Usage
+ *
+ * Creates a KMS key used to encrypt or decrypt data used by Lambda
+ *
+ * ```hcl
+ * module "lambda_kms_key" {
+ *   source = "dod-iac/lambda-kms-key/aws"
+ *
+ *   tags = {
+ *     Application = var.application
+ *     Environment = var.environment
+ *     Automation  = "Terraform"
+ *   }
+ * }
+ * ```
+ *
+ * ## Terraform Version
+ *
+ * Terraform 0.12 and 0.13. Pin module version to ~> 1.0.0 . Submit pull-requests to master branch.
+ *
+ * Terraform 0.11 is not supported.
+ *
+ * ## License
+ *
+ * This project constitutes a work of the United States Government and is not subject to domestic copyright protection under 17 USC § 105.  However, because the project utilizes code licensed from contributors and other third parties, it therefore is licensed under the MIT License.  See LICENSE file for more information.
+ */
+
+data "aws_caller_identity" "current" {}
+
+data "aws_partition" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_iam_policy_document" "lambda" {
+  policy_id = "key-policy-lambda"
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        format(
+          "arn:%s:iam::%s:root",
+          data.aws_partition.current.partition,
+          data.aws_caller_identity.current.account_id
+        )
+      ]
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid = "AllowLambda"
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*"
+    ]
+    effect = "Allow"
+    principals {
+      type = "Service"
+      identifiers = [
+        format(
+          "lambda.%s.amazonaws.com",
+          data.aws_region.current.name,
+        )
+      ]
+    }
+    resources = ["*"]
+  }
+}
+
+resource "aws_kms_key" "lambda" {
+  description             = var.description
+  deletion_window_in_days = var.key_deletion_window_in_days
+  enable_key_rotation     = "true"
+  policy                  = data.aws_iam_policy_document.lambda.json
+  tags                    = var.tags
+}
+
+resource "aws_kms_alias" "lambda" {
+  name          = var.name
+  target_key_id = aws_kms_key.lambda.key_id
+}
